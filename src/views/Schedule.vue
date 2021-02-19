7b657652e990c88e3d1ac4340b5c5c34b5acc633
Seantsang <9394wing@gmail.com>
2021-02-19 21:14:46 +0800

    schedule

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -10,8 +10,15 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import WithHeaderFooter from "@/layout/WithHeaderFooter.vue";
+import { useI18n } from "vue-i18n";
 export default defineComponent({
   components: { WithHeaderFooter },
+  setup() {
+    const { t } = useI18n();
+    return {
+      t,
+    };
+  },
 });
 </script>
 
