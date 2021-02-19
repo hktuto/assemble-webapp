66678007454cd6af516769e064b8008e6f6e74f2
KaKa <kaka@kakawebsitedemo.com>
2021-02-19 20:20:32 +0800

    feat: login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,19 @@
 <template>
   <WithHeaderFooter>
     <div class="pageContent">
-      Login
+      <h3 class="header">{{ t("login.title") }}</h3>
+      <IonInput placeholder="Phone Number"></IonInput>
+      <IonInput placeholder="Password"></IonInput>
+      <IonButton mode="ios">
+        {{ t("action.login") }}
+      </IonButton>
+      <span>
+        {{ t("login.forgot-password") }}
+      </span>
+      <span>
+        {{ t("login.register1") }}
+        {{ t("login.register2") }}
+      </span>
     </div>
     <div class="bgContainer"></div>
   </WithHeaderFooter>
@@ -10,9 +22,21 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import WithHeaderFooter from "@/layout/WithHeaderFooter.vue";
+import { IonInput, IonButton } from "@ionic/vue";
+import { useI18n } from "vue-i18n";
 export default defineComponent({
-  components: { WithHeaderFooter },
+  components: { WithHeaderFooter, IonInput, IonButton },
+  setup() {
+    const { t } = useI18n();
+    return {
+      t,
+    };
+  },
 });
 </script>
 
-<style></style>
+<style>
+.header {
+  align-self: flex-start;
+}
+</style>
