eb9bab59b4b829ab037015e1310c77ee13d04f47
KaKa <kaka@kakawebsitedemo.com>
2021-02-19 21:56:13 +0800

    feat: my profile

--- src/views/MyProfile.vue
+++ src/views/MyProfile.vue
@@ -1,6 +1,20 @@
 <template>
   <WithHeaderFooter>
-    <div class="pageContent">My Profile</div>
+    <div class="pageContent">
+      <h3 class="header">{{ t("myprofile.title") }}</h3>
+      <IonInput :placeholder="t('myprofile.firstname')"></IonInput>
+      <IonInput :placeholder="t('myprofile.lastname')"></IonInput>
+      <IonInput :placeholder="t('myprofile.mobile')"></IonInput>
+      <IonInput :placeholder="t('myprofile.email')"></IonInput>
+      <IonInput :placeholder="t('myprofile.district')"></IonInput>
+      <IonTextarea :placeholder="t('myprofile.address')"></IonTextarea>
+      <IonButton mode="ios">
+        {{ t("action.update") }}
+      </IonButton>
+      <IonButton mode="ios">
+        {{ t("action.changeplan") }}
+      </IonButton>
+    </div>
     <div class="bgContainer"></div>
   </WithHeaderFooter>
 </template>
@@ -8,8 +22,21 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import WithHeaderFooter from "@/layout/WithHeaderFooter.vue";
+import { useI18n } from "vue-i18n";
+import { IonButton, IonInput, IonTextarea } from "@ionic/vue";
 export default defineComponent({
-  components: { WithHeaderFooter },
+  components: {
+    WithHeaderFooter,
+    IonInput,
+    IonTextarea,
+    IonButton,
+  },
+  setup() {
+    const { t } = useI18n();
+    return {
+      t,
+    };
+  },
 });
 </script>
 
